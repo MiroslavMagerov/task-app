2dd06591aa2cd0c170842791f69cca9ee06b2327
Miroslav <miroslav.magerov@gmail.com>
2024-09-24 23:20:34 +0200

    Updated register and login form to display errors and added log out functionality

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/login.css';
 
 export const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isLogged, setIsLogged] = useState(false);
+  const [loginOrRegister, setLoginOrRegister] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
 
   const BACKEND_API = 'https://task-app-3ois.onrender.com/users';
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   const handleSubmitForm = async (e) => {
     e.preventDefault();
@@ -17,13 +25,14 @@ export const Login = () => {
 
     if (username.length < 5 || password.length < 5) {
       setError('Username y Password deben tener al menos 5 carácteres');
+      return;
     }
 
-    const endpoint = isLogged ? 'login' : 'register';
+    const endpoint = loginOrRegister ? 'login' : 'register';
 
     console.log(
       `Intentando ${
-        isLogged ? 'iniciar sesión' : 'registrar'
+        loginOrRegister ? 'iniciar sesión' : 'registrar'
       } con el usuario: ${username}`
     );
 
@@ -35,18 +44,21 @@ export const Login = () => {
       });
 
       console.log(
-        `Respuesta del servidor para ${isLogged ? 'login' : 'register'}:`,
+        `Respuesta del servidor para ${
+          loginOrRegister ? 'login' : 'register'
+        }:`,
         resp
       );
 
       const data = await resp.json();
 
       if (!resp.ok) {
-        console.error(data.message || 'Error');
+        console.error(data.message || 'Error en el servidor');
+        setError(data.message);
         return;
       }
 
-      if (!isLogged) {
+      if (!loginOrRegister) {
         setSuccessMessage('Registro exitoso. Ahora puedes iniciar sesión.');
         return;
       }
@@ -54,36 +66,53 @@ export const Login = () => {
       localStorage.setItem('token', data.token);
       console.log('JWT generado:', data.token);
       setSuccessMessage('Login exitoso');
+      setIsAuthenticated(true);
     } catch (error) {
-      console.error(error);
       setError('Error de conexión. Inténtalo más tarde.');
+      console.error(error);
     }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
   };
 
   return (
     <main>
-      <form onSubmit={handleSubmitForm}>
-        <h2>{isLogged ? 'Login form' : 'Register form'}</h2>
-        {error && <p className='error'>{error}</p>}
-
-        <input
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder='Username'
-        />
-        <input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder='Password'
-        />
-        {successMessage && <p className='success'>{successMessage}</p>}
-        <button type='submit'>{isLogged ? 'Login' : 'Register'}</button>
-        <button type='button' onClick={() => setIsLogged(!isLogged)}>
-          {isLogged ? 'Switch to register' : 'Switch to login'}
-        </button>
-      </form>
+      {isAuthenticated ? (
+        <>
+          <h2 className='color-white'>You are logged in</h2>
+          <button onClick={handleLogout}>Log out</button>
+        </>
+      ) : (
+        <form onSubmit={handleSubmitForm}>
+          <h2>{loginOrRegister ? 'Login form' : 'Register form'}</h2>
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder='Username'
+          />
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Password'
+          />
+          {successMessage && <p className='success'>{successMessage}</p>}
+          {error && <p className='error'>{error}</p>}
+          <button type='submit'>
+            {loginOrRegister ? 'Login' : 'Register'}
+          </button>
+          <button
+            type='button'
+            onClick={() => setLoginOrRegister(!loginOrRegister)}
+          >
+            {loginOrRegister ? 'Switch to register' : 'Switch to login'}
+          </button>
+        </form>
+      )}
     </main>
   );
 };
---
+++ frontend/src/components/TaskList.jsx
@@ -0,0 +1,8 @@
+export const TaskList = () => {
+  return (
+    <div className='task-list'>
+      <h1>Task List</h1>
+      <code>Only logged users can see this page</code>
+    </div>
+  );
+};
