2cfa720ff212d339896fc7663ff3d0940fbd5595
Miroslav <miroslav.magerov@gmail.com>
2024-09-27 13:33:24 +0200

    Updated navbar style and default general font

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,26 +2,28 @@ import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
 
 export const Navbar = () => {
+  const handleTasksClick = (e) => {
+    if (!localStorage.getItem('token')) {
+      alert('You need to be logged in to be able to see the tasks page.');
+      e.preventDefault();
+    }
+  };
   const handleLogout = () => {
     localStorage.removeItem('token');
   };
 
-  const handleTasksClick = (e) => {
-    if (!localStorage.getItem('token')) {
-      alert('Por favor, inicia sesión para acceder a esta página.');
-      e.preventDefault();
-    }
-  };
-
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/tasks' onClick={handleTasksClick}>
-        Tasks
-      </Link>
-      <Link to='/about'>About</Link>
-      <button onClick={handleLogout}>Logout</button>
+      <h1>TASKLY</h1>
+      <div className='links-div'>
+        <Link to='/'>Home</Link>
+        <Link to='/tasks' onClick={handleTasksClick}>
+          Tasks
+        </Link>
+        <Link to='/about'>About</Link>
+        <Link to='/login'>Login</Link>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
     </nav>
   );
 };
