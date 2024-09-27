1d7ffc9770af77c38bb9c5319e470c0448110dc1
Miroslav <miroslav.magerov@gmail.com>
2024-09-27 19:51:28 +0200

    Added keepAlive.js script to keep alive the backend hosting without it going to sleep

---
+++ backend/keepAlive.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+
+const KEEP_ALIVE_URL = 'https://task-app-3ois.onrender.com/users';
+const INTERVAL = 10 * 60 * 1000;
+
+const keepAlive = async () => {
+  try {
+    const response = await fetch(KEEP_ALIVE_URL);
+    if (response.ok) {
+      console.log(
+        'Keep-alive request successful:',
+        new Date().toLocaleString()
+      );
+    } else {
+      console.error('Keep-alive request failed:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error in keep-alive request:', error);
+  }
+};
+
+setInterval(keepAlive, INTERVAL);
+
+keepAlive();
