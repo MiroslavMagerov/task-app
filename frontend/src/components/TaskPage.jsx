4dc455578f6513c97991506a97c7c2f959ba2cdf
Miroslav <miroslav.magerov@gmail.com>
2024-09-29 11:41:27 +0200

    Fixed error when no available tasks to display

--- frontend/src/components/TaskPage.jsx
+++ frontend/src/components/TaskPage.jsx
@@ -9,13 +9,20 @@ export const TaskPage = () => {
       <h1>Task List</h1>
       {useTasksHook.isLoading ? (
         <h2>Loading...</h2>
+      ) : useTasksHook.tasks.length === 0 ? (
+        <h2>Empty tasks</h2>
       ) : (
-        useTasksHook.tasks.map((task) => (
-          <li key={task.title}>
-            {task.title} - Created by {task.createdBy} at {task.createdAt}.
-            Status {task.completed}
-          </li>
-        ))
+        <ul>
+          {useTasksHook.tasks.map((task) => (
+            <li key={task._id}>
+              {' '}
+              {/* Cambia `task.title` por `task._id` si estás usando IDs únicos */}
+              {task.title} - Created by {task.createdBy} at{' '}
+              {new Date(task.createdAt).toLocaleString()}. Status:{' '}
+              {task.completed ? 'Completed' : 'Not completed'}
+            </li>
+          ))}
+        </ul>
       )}
 
       <button onClick={() => useTasksHook.createTask('Default task')}>
